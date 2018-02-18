66c66e6a293b14db5abe3648f6e92e92f14328ec
Conor Mckeown <cpmmckeown@gmail.com>
2018-02-18 15:07:50 +0000

    I have completed Chapter 5 and run the test program; I received no errors with errors being detected for Chapter 6 onwards

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+	
+	def __str__(self): # For Python 2, use __unicode__ too 
+		return self.title 
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
