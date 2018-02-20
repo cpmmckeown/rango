53d34416de49d01bb07fb6cece7392dd3dc11687
Conor Mckeown <cpmmckeown@gmail.com>
2018-02-20 21:32:42 +0000

    Completed Chapter 10 - quite sure completed everything

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,6 +8,33 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout  
+from datetime import datetime 
+
+#helper method
+def get_server_side_cookie(request, cookie, default_value=None):
+	val = request.session.get(cookie) 
+	if not val: 
+		val = default_value 
+	return val 
+	
+#added in chpt 10 - cookies	
+def visitor_cookie_handler(request):
+	#get # of visitors to site
+	vists = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+										'%Y-%m-%d %H:%M:%S')
+	#if more than a day since last visit
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		#update last visit cookie
+		request.session['last_visit'] = str(datetime.now()) 
+	else:
+		visits = 1 
+		#set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+	#update/set the visits cookie	
+	request.session['visits'] = visits 
 
 
 def index(request):
@@ -16,15 +43,24 @@ def index(request):
 	#retrieve top 5 only or all if less than 5
 	#place list in context_dict
 	#that will be passed to template engine
+	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': pages_list}
+	
+	#chpt 10 - call function to handle the cookies
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits'] 
 	
 	#Render response, send it back
 	response = render(request, 'rango/index.html', context=context_dict)
 	return response
 	
 def about(request):
+
+	if request.session.test_cookie_worked(): 
+		print("TEST COOKIE WORKED!") 
+		request.session.delete_test_cookie() 
 	return render(request, 'rango/about.html', {}) 
 	# The '{}' at the end is an empty dictionary because we have 
 	# no additional data to give the template.
@@ -201,5 +237,5 @@ def user_logout(request):
 # Since we know the user is logged in, we can now just log them out. logout(request) 
 # Take the user back to the homepage. 
 	return HttpResponseRedirect(reverse('index')) 
-		
-		
+
+
